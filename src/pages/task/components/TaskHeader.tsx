aea12d73d076e18be737a8d86c1455b19c97662e
chenmengqi <chenmengqi@powerlaw.ai>
2025-03-06 14:36:07 +0800

    feat(待办任务): 动画优化

--- src/pages/task/components/TaskHeader.tsx
+++ src/pages/task/components/TaskHeader.tsx
@@ -1,13 +1,26 @@
 import { Ellipsis } from "@react-vant/icons";
 import dogIcon from "@/assets/dogIcon.svg";
-import './taskHeader.scss'
+import "./taskHeader.scss";
+import { useState } from "react";
 
 const taskHeaderText = "待办任务";
 const TaskHeader = () => {
+  const [isJumping, setIsJumping] = useState(false);
+  const handleLogoClick = () => {
+    setIsJumping(true);
+    setTimeout(() => {
+      setIsJumping(false);
+    }, 1000);
+  };
   return (
     <div className="flex justify-between items-center h-20 px-4 bg-green-100 text-3xl">
       <div>
-        <img src={dogIcon} alt="" className="w-12 h-12 logo"  />
+        <img
+          src={dogIcon}
+          alt=""
+          className={(isJumping ? "logo-active" : "") + " w-12 h-12 logo"}
+          onClick={handleLogoClick}
+        />
       </div>
       <div className="text-3xl">{taskHeaderText}</div>
       <div>
