f2c358cdb5a62f4c65a28a3c18aed8d0df9b8e60
chenmengqi <chenmengqi@powerlaw.ai>
2025-03-11 10:34:03 +0800

    feat(路由): 新增重定向

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,11 +1,18 @@
 import { lazy } from "react";
 import { RouteConfig } from "./index";
+import { Navigate, redirect } from "react-router-dom";
 
 const routes: RouteConfig[] = [
   {
     path: "/",
     element: lazy(() => import("@/pages/MainLayout")),
     children: [
+      // 添加 index 路由实现自动重定向
+      {
+        index: true,
+        loader: () => redirect("/task"),
+        element: Navigate,
+      },
       {
         path: "/task",
         element: lazy(() => import("@/pages/task/TaskBoard")),
