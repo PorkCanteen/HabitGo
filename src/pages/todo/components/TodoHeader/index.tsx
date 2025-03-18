171187a41b0ba92bc5765198c636c01d02e1780f
chenmengqi <2385190394@qq.com>
2025-03-18 20:56:09 +0800

    feat(待办): 待办显示logo

--- src/pages/todo/components/TodoHeader/index.tsx
+++ src/pages/todo/components/TodoHeader/index.tsx
@@ -1,10 +1,35 @@
+import dogIcon from "@/assets/dogIconBold.png";
+import pigIcon from "@/assets/pigIconBold.png";
+import { useState } from "react";
+import "./index.scss";
 const todoHeaderText = "我的待办";
 const TodoHeader = () => {
+  const [isJumping, setIsJumping] = useState(false);
+  const handleLogoClick = () => {
+    setIsJumping(true);
+    setTimeout(() => {
+      setIsJumping(false);
+    }, 1000);
+  };
   return (
     <div className="flex justify-between items-center h-20 px-4 bg-green-100 text-3xl">
-      <div></div>
+      <div>
+        <img
+          src={pigIcon}
+          alt=""
+          className={(isJumping ? "logo-active" : "") + " w-16 h-16 logo"}
+          onClick={handleLogoClick}
+        />
+      </div>
       <div className="text-3xl">{todoHeaderText}</div>
-      <div></div>
+      <div>
+        <img
+          src={dogIcon}
+          alt=""
+          className={(isJumping ? "logo-active" : "") + " w-14 h-14 logo"}
+          onClick={handleLogoClick}
+        />
+      </div>
     </div>
   );
 };
