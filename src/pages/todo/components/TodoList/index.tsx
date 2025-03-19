003517930ef8c279c7c410eac469968185552c4d
chenmengqi <2385190394@qq.com>
2025-03-19 20:44:44 +0800

    feat(待办): 待办tab点击过滤

--- src/pages/todo/components/TodoList/index.tsx
+++ src/pages/todo/components/TodoList/index.tsx
@@ -2,6 +2,7 @@ import { forwardRef, useImperativeHandle, useState } from "react";
 import { List } from "react-vant";
 import TodoItem from "../TodoItem";
 import { useHttp } from "@/hooks/useHttp";
+import "./index.scss";
 
 // 习惯项
 export interface Todo {
@@ -16,22 +17,26 @@ export interface Todo {
 const TodoList = forwardRef((props, ref) => {
   const { sendRequest } = useHttp();
   const [finished, setFinished] = useState(false);
-  // 初始待办数据
   const [todos, setTodos] = useState<Todo[]>([]);
-  
+  const [activeTab, setActiveTab] = useState("全部"); // 新增状态管理
+  const [allTodos, setAllTodos] = useState<Todo[]>([]);  // 新增状态保存所有数据
+
+  const tabs = ["全部", "紧急", "常规"]; // 新增tab列表
+
   const fetchData = async () => {
     const res: any = await sendRequest({
       url: "/todo/list",
       method: "GET",
     });
     if (res.data && res.data.length) {
-      setTodos(res.data);
+      setAllTodos(res.data);  // 保存所有数据
+      setTodos(res.data);     // 初始化显示所有数据
     }
     setFinished(true);
   };
 
   useImperativeHandle(ref, () => ({
-    fetchData
+    fetchData,
   }));
 
   // 处理习惯点击
@@ -50,21 +55,45 @@ const TodoList = forwardRef((props, ref) => {
   };
 
   const onListLoad = async () => {
-    await fetchData()
+    await fetchData();
   };
 
+  const tabFilters = {
+    "全部": (todos: Todo[]) => todos,
+    "紧急": (todos: Todo[]) => todos.filter(todo => todo.type === 1),
+    "常规": (todos: Todo[]) => todos.filter(todo => todo.type === 2),
+  };
+
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+    const filter = tabFilters[tab as keyof typeof tabFilters];
+    setTodos(filter(allTodos));
+  };
   return (
-    <div className=" p-2 h-full overflow-y-auto">
-      <List onLoad={onListLoad} finished={finished}>
-        {todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            todoClick={() => todo.id !== undefined && handleClick(todo.id)}
-            updateList={fetchData}
-          ></TodoItem>
+    <div className="h-full">
+      <div className="flex pl-2 mt-2">
+        {tabs.map((tab) => (
+          <div
+            key={tab}
+            className={`tab-card ${activeTab === tab ? "checked" : ""}`}
+            onClick={() => handleTabClick(tab)}
+          >
+            {tab}
+          </div>
         ))}
-      </List>
+      </div>
+      <div className=" p-2 h-full overflow-y-auto">
+        <List onLoad={onListLoad} finished={finished}>
+          {todos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              todoClick={() => todo.id !== undefined && handleClick(todo.id)}
+              updateList={fetchData}
+            ></TodoItem>
+          ))}
+        </List>
+      </div>
     </div>
   );
 });
