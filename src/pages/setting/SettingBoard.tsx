2d8e4801e8c85e34a318be935dff960d3a3812a5
chenmengqi <2385190394@qq.com>
2025-03-30 12:00:18 +0800

    feat(登录): 从cookie获取用户昵称

--- src/pages/setting/SettingBoard.tsx
+++ src/pages/setting/SettingBoard.tsx
@@ -1,12 +1,17 @@
 import dogIcon from "@/assets/dogIconBold.png";
 import "./SettingBoard.scss";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Cookies from 'js-cookie';
 
 const SettingBoard = () => {
+  const nickname = useState(Cookies.get('user') ? JSON.parse(Cookies.get('user') as string).nickname : '')
   const navigate = useNavigate();
   const handleLogout = () => {
     navigate("/login");
   };
+
+  
   return (
     <div className="setting-board p-4 flex flex-col items-center justify-center">
       <div className="avatar-container mb-4">
@@ -16,7 +21,7 @@ const SettingBoard = () => {
           className="avatar w-60 h-60 rounded-full bg-white"
         />
       </div>
-      <div className="username text-3xl font-medium mb-12">肥肠粉加节子</div>
+      <div className="nickname text-3xl font-medium mb-12">{nickname}</div>
       <div className="button-container flex flex-col gap-3 w-full max-w-xs items-center">
         <button
           className="logout-button bg-white text-gray-700 border border-gray-300 px-6 py-3 rounded-md hover:bg-gray-100 transition-colors text-xl"
