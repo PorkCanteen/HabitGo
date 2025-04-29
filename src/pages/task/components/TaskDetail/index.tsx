5c55754f33b0dd87e2daf3cb94bd881deceb3b13
chenmengqi <chenmengqi@powerlaw.ai>
2025-04-29 16:26:39 +0800

    feat(习惯): 习惯详情页-布局

--- src/pages/task/components/TaskDetail/index.tsx
+++ src/pages/task/components/TaskDetail/index.tsx
@@ -5,21 +5,32 @@ const TaskDetail = () => {
   // 模拟一些打卡记录
   const [completedDates] = useState<string[]>([
     // 过去几天的随机打卡记录
-    dayjs().format('YYYY-MM-DD'),
-    dayjs().subtract(1, 'day').format('YYYY-MM-DD'),
-    dayjs().subtract(3, 'day').format('YYYY-MM-DD'),
-    dayjs().subtract(5, 'day').format('YYYY-MM-DD'),
-    dayjs().subtract(7, 'day').format('YYYY-MM-DD'),
-    dayjs().subtract(10, 'day').format('YYYY-MM-DD'),
+    dayjs().format("YYYY-MM-DD"),
+    dayjs().subtract(1, "day").format("YYYY-MM-DD"),
+    dayjs().subtract(3, "day").format("YYYY-MM-DD"),
+    dayjs().subtract(5, "day").format("YYYY-MM-DD"),
+    dayjs().subtract(7, "day").format("YYYY-MM-DD"),
+    dayjs().subtract(10, "day").format("YYYY-MM-DD"),
     // 上个月的一些日期
-    dayjs().subtract(1, 'month').date(5).format('YYYY-MM-DD'),
-    dayjs().subtract(1, 'month').date(10).format('YYYY-MM-DD'),
-    dayjs().subtract(1, 'month').date(15).format('YYYY-MM-DD'),
-    dayjs().subtract(1, 'month').date(20).format('YYYY-MM-DD'),
+    dayjs().subtract(1, "month").date(5).format("YYYY-MM-DD"),
+    dayjs().subtract(1, "month").date(10).format("YYYY-MM-DD"),
+    dayjs().subtract(1, "month").date(15).format("YYYY-MM-DD"),
+    dayjs().subtract(1, "month").date(20).format("YYYY-MM-DD"),
   ]);
-  return <div>
-    <Calendar highlightDates={completedDates} />
-  </div>;
+  return (
+    <div>
+      {/* 标题 */}
+      <div className="header-container"></div>
+      {/* 目标 */}
+      <div className="target-container"></div>
+      {/* 日历 */}
+      <div className="calendar-container">
+        <Calendar highlightDates={completedDates} />
+      </div>
+      {/* 统计 */}
+      <div className="statics-container"></div>
+    </div>
+  );
 };
 
 export default TaskDetail;
