6a33b2bd8cf9529d411d8abb5378e0c926c57738
chenmengqi <chenmengqi@powerlaw.ai>
2025-07-01 10:36:17 +0800

    style(样式): 待办列表-header优化

--- src/pages/task/components/TaskHeader/index.tsx
+++ src/pages/task/components/TaskHeader/index.tsx
@@ -3,7 +3,7 @@ import "./index.scss";
 const taskHeaderText = "我的习惯";
 const TaskHeader = () => {
   return (
-    <div className="header-container flex justify-center items-center h-24 px-4 text-3xl shrink-0">
+    <div className="task-header-container flex justify-center items-center h-24 px-4 text-3xl shrink-0">
       <div className="title font-bold">{taskHeaderText}</div>
     </div>
   );
--- src/pages/todo/components/TodoHeader/index.tsx
+++ src/pages/todo/components/TodoHeader/index.tsx
@@ -1,39 +1,10 @@
-import { useState } from "react";
 import "./index.scss";
+
 const todoHeaderText = "我的待办";
 const TodoHeader = () => {
-  const [isJumping, setIsJumping] = useState(false);
-  const handleLogoClick = () => {
-    setIsJumping(true);
-    setTimeout(() => {
-      setIsJumping(false);
-    }, 1000);
-  };
   return (
-    <div className="header-container flex justify-between items-center h-24 px-4 text-3xl shrink-0">
-      <div>
-        <svg
-          onClick={handleLogoClick}
-          aria-hidden="true"
-          width={18}
-          height={18}
-          className={(isJumping ? "logo-active" : "") + " w-14 h-14 logo"}
-        >
-          <use xlinkHref="#icon-xiangsu_caomei"></use>
-        </svg>
-      </div>
-      <div className="text-4xl font-bold">{todoHeaderText}</div>
-      <div>
-        <svg
-          onClick={handleLogoClick}
-          aria-hidden="true"
-          width={18}
-          height={18}
-          className={(isJumping ? "logo-active" : "") + " w-14 h-14 logo"}
-        >
-          <use xlinkHref="#icon-xiangsu_caomei"></use>
-        </svg>
-      </div>
+    <div className="todo-header-container flex justify-center items-center h-24 px-4 text-3xl shrink-0">
+      <div className="title font-bold">{todoHeaderText}</div>
     </div>
   );
 };
